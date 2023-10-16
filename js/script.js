7c35bb30da03fecce46a9f387b32062c40866f2f
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-16 18:14:02 +0200

    add while

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@
 //quindi creiamo un ciclo while che dovrà avere questa condizione(array !== dalla parola stop)
 //ogni qualvolta l'utente inserirà un elemento, dovremmo usare .push per metterlo dentro l'array
 //creiamo un ciclo for per stampare i singoli elementi
+
+//array vuoto
+let list = [];
+//inizializziamo la variabile del prompt
+let item = '';
+
+//ciclo while inizierà per cerca una parola diversa da stop
+while(item != 'stop'){ 
+    item = prompt('aggiungi alla lista');
+    console.log(item);
+    //usiamo il trim per evitare spazi vuoti
+    item = item.trim();
+    //condizione se 
+    if (item == 'stop'){
+        list.push(item);
+    }
+};
