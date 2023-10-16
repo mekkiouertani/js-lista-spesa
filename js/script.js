3891c3551bd8d5358781d6324ca072910bb86752
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-16 18:09:12 +0200

    add comment to js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//chiediamo all'utente i prodotti da acquistare tramite prompt
+//il prompt dovrà interrompersi con la parola 'stop'
+//creiamo un array vuoto
+//quindi creiamo un ciclo while che dovrà avere questa condizione(array !== dalla parola stop)
+//ogni qualvolta l'utente inserirà un elemento, dovremmo usare .push per metterlo dentro l'array
+//creiamo un ciclo for per stampare i singoli elementi
